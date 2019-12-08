0a9c42016e7a35d5af3e4e8b63bffdcade45f3dd
Paresh Sharma <Paresh.Sharma@Valaries-MacBook-Pro.local>
2019-12-08 08:22:17 -0800

    Update styles.css and Index.js

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-//jshint esversion: 6
-
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const app = express();
-
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
-
-// I am having trouble executing this code. It was working before I added
-// bootstrap and CSS for designing (design is still under development).
-// I have been doing a lot of research but cannot figure out the problem.
-
-app.post("/", function(req, res) {
-  let vancomycin = parseFloat(req.body.vancomycin);
-  let diluent = parseFloat(req.body.diluent);
-  let ancillary = parseFloat(req.body.ancillary);
-  let waste = parseFloat(req.body.waste);
-  let tHourly = parseFloat(req.body.tHourly);
-  let pHourly = parseFloat(req.body.pHourly);
-  let tMinutes = parseFloat(req.body.tMinutes);
-  let pMinutes = parseFloat(req.body.pMinutes);
-
+var returnCostPerDose = function(e) {
+  e.preventDefault();
+  let vancomycin = parseFloat(document.getElementById("vancomycinCost").value);
+  let diluent = parseFloat(document.getElementById("diluentCost").value);
+  let ancillary = parseFloat(document.getElementById("ancillaryCost").value);
+  let waste = parseFloat(document.getElementById("wastePercentage").value);
+  let tHourly = parseFloat(document.getElementById("tHourlyCost").value);
+  let pHourly = parseFloat(document.getElementById("pHourlyCost").value);
+  let tMinutes = parseFloat(document.getElementById("tMinutesCost").value);
+  let pMinutes = parseFloat(document.getElementById("pMinutesCost").value);
   let supplyCost = vancomycin + diluent + ancillary;
   let laborCost = (pHourly * pMinutes / 60) + (tHourly * tMinutes / 60);
-
-  let totalCost = (supplyCost + laborCost) * (1 + waste);
-
-  res.send("Total cost per dose is " + totalCost.toFixed(2));
-});
-
-app.listen(3000, function() {
-  console.log("Server is running on port 3000.")
-});
+  let costPerDose = (supplyCost + laborCost) * (1 + (waste / 100));
+  document.getElementById("output").innerHTML = "$" + costPerDose.toFixed(2);
+}
